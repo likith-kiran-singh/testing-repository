0aa0dd69d8c96f0adc13b5da0a646c8cb1f15a22
Likith Kiran Singh Bondili <113082870+likith-kiran-singh@users.noreply.github.com>
2022-10-27 10:06:15 +0530

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,12 @@
+/*Aim: Write a Java Program to create an abstract class named Shape that contains
+two integers and an empty method named print Area(). Provide three classes
+named Rectangle, Triangle and Circle such that each one of the classes extends
+the class Shape. Each one of the classes contains only the method print Area ()
+that prints the area of the given shape. */
+abstract class Shape
+{
+int a=3,b=4;
+abstract public void printarea();
+} 
+
+
